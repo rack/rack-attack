f7a1eac521c61961bbe08ab20059e92d2639b38b
Aaron Suggs <aaron@ktheory.com>
2012-08-15 18:15:49 -0400

    Cache key time must be a fixnum

--- lib/rack/attack/cache.rb
+++ lib/rack/attack/cache.rb
@@ -11,7 +11,7 @@ module Rack
       def count(unprefixed_key, period)
         epoch_time = Time.now.to_i
         expires_in = period - (epoch_time % period)
-        key = "#{prefix}:#{epoch_time/period}:#{unprefixed_key}"
+        key = "#{prefix}:#{(epoch_time/period).to_i}:#{unprefixed_key}"
         result = store.increment(key, 1, :expires_in => expires_in)
         # NB: Some stores return nil when incrementing uninitialized values
         if result.nil?
