aeca8e52b39cb865ceca361ac3392ed61a266c84
Gonzalo <grzuy0@gmail.com>
2018-01-17 11:05:22 -0300

    Update to new ruby hash syntax in docs

--- examples/rack_attack.rb
+++ examples/rack_attack.rb
@@ -3,15 +3,15 @@
 # Throttle 10 requests/ip/second
 # NB: return value of block is key name for counter
 #     falsy values bypass throttling
-Rack::Attack.throttle("req/ip", :limit => 10, :period => 1) { |req| req.ip }
+Rack::Attack.throttle("req/ip", limit: 10, period: 1) { |req| req.ip }
 
 # Throttle attempts to a particular path. 2 POSTs to /login per second per IP
-Rack::Attack.throttle "logins/ip", :limit => 2, :period => 1 do |req|
+Rack::Attack.throttle "logins/ip", limit: 2, period: 1 do |req|
   req.post? && req.path == "/login" && req.ip
 end
 
 # Throttle login attempts per email, 10/minute/email
-Rack::Attack.throttle "logins/email", :limit => 2, :period => 60 do |req|
+Rack::Attack.throttle "logins/email", limit: 2, period: 60 do |req|
   req.post? && req.path == "/login" && req.params['email']
 end
 
