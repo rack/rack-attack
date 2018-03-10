69a2fd4055d469d48dca96572e718c014bb139d5
Lucas Mansur <lucas.mansur2@gmail.com>
2018-03-10 11:47:29 -0300

    Refactor retry_after logic

--- lib/rack/attack.rb
+++ lib/rack/attack.rb
@@ -121,19 +121,25 @@ class Rack::Attack
       blocklisted_response
     end
 
+    def retry_after(period)
+      if period
+        (period - (Time.now.to_i % period)).to_s
+      else
+        ''
+      end
+    end
   end
 
   # Set defaults
   @notifier             = ActiveSupport::Notifications if defined?(ActiveSupport::Notifications)
-  @blocklisted_response = lambda {|env| [403, {'Content-Type' => 'text/plain'}, ["Forbidden\n"]] }
-  @throttled_response   = lambda {|env|
+  @blocklisted_response = lambda { |env| [403, {'Content-Type' => 'text/plain'}, ["Forbidden\n"]] }
+  @throttled_response   = lambda { |env|
     period = (env['rack.attack.match_data'] || {})[:period]
-    retry_after = if period
-                    period - (Time.now.to_i % period)
-                  else
-                    ''
-                  end
-    [429, {'Content-Type' => 'text/plain', 'Retry-After' => retry_after.to_s}, ["Retry later\n"]]
+    [
+      429,
+      { 'Content-Type' => 'text/plain', 'Retry-After' => retry_after(period) },
+      ["Retry later\n"]
+    ]
   }
 
   def initialize(app)
