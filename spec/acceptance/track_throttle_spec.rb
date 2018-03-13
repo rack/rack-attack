564cbedb36693e026a981512197e9e0702c152e0
Gonzalo Rodriguez <grzuy0@gmail.com>
2018-03-13 18:27:19 -0300

    Acceptance test that tracking throttles doesn't actually throttle requests

--- spec/acceptance/track_throttle_spec.rb
+++ spec/acceptance/track_throttle_spec.rb
@@ -2,7 +2,7 @@ require_relative "../spec_helper"
 require "timecop"
 
 describe "#track with throttle-ish options" do
-  it "notifies when throttle goes over the limit" do
+  it "notifies when throttle goes over the limit without actually throttling requests" do
     Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
     Rack::Attack.track("by ip", limit: 1, period: 60) do |request|
@@ -22,15 +22,21 @@ describe "#track with throttle-ish options" do
     assert_nil notification_matched
     assert_nil notification_type
 
+    assert_equal 200, last_response.status
+
     get "/", {}, "REMOTE_ADDR" => "5.6.7.8"
 
     assert_nil notification_matched
     assert_nil notification_type
 
+    assert_equal 200, last_response.status
+
     get "/", {}, "REMOTE_ADDR" => "1.2.3.4"
 
     assert_equal "by ip", notification_matched
     assert_equal :track, notification_type
+
+    assert_equal 200, last_response.status
 
     Timecop.travel(60) do
       notification_matched = nil
@@ -40,6 +46,8 @@ describe "#track with throttle-ish options" do
 
       assert_nil notification_matched
       assert_nil notification_type
+
+      assert_equal 200, last_response.status
     end
   end
 end
