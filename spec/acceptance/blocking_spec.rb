21fe32b895c61daf03aebde3a4244bb43720ff03
Gonzalo Rodriguez <grzuy0@gmail.com>
2018-03-26 16:00:32 -0300

    Acceptance test ability to subscribe to blocking events

--- spec/acceptance/blocking_spec.rb
+++ spec/acceptance/blocking_spec.rb
@@ -18,4 +18,24 @@ describe "#blocklist" do
 
     assert_equal 200, last_response.status
   end
+
+  it "notifies when the request is blocked" do
+    notification_matched = nil
+    notification_type = nil
+
+    ActiveSupport::Notifications.subscribe("rack.attack") do |_name, _start, _finish, _id, request|
+      notification_matched = request.env["rack.attack.matched"]
+      notification_type = request.env["rack.attack.match_type"]
+    end
+
+    get "/", {}, "REMOTE_ADDR" => "5.6.7.8"
+
+    assert_nil notification_matched
+    assert_nil notification_type
+
+    get "/", {}, "REMOTE_ADDR" => "1.2.3.4"
+
+    assert_equal "block 1.2.3.4", notification_matched
+    assert_equal :blocklist, notification_type
+  end
 end
