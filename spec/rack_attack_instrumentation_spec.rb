58aa6714d898d7c11fbe048efed09043d330b5eb
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-02-24 20:16:51 -0300

    test: let test file be run individually

--- spec/rack_attack_instrumentation_spec.rb
+++ spec/rack_attack_instrumentation_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "spec_helper"
+
 # ActiveSupport::Subscribers added in ~> 4.0.2.0
 if ActiveSupport::VERSION::MAJOR > 3
   require_relative 'spec_helper'
