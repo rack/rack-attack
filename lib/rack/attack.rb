3639afc196249c1b143327c559086af5a9d55bc3
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-03-01 22:01:17 -0300

    refactor: remove unnecessary block local variable

--- lib/rack/attack.rb
+++ lib/rack/attack.rb
@@ -50,13 +50,11 @@ class Rack::Attack
     end
 
     def blocklist_ip(ip_address)
-      ip_blocklist_proc = lambda { |request| IPAddr.new(ip_address).include?(IPAddr.new(request.ip)) }
-      anonymous_blocklists << Blocklist.new(nil, &ip_blocklist_proc)
+      anonymous_blocklists << Blocklist.new(nil) { |request| IPAddr.new(ip_address).include?(IPAddr.new(request.ip)) }
     end
 
     def safelist_ip(ip_address)
-      ip_safelist_proc = lambda { |request| IPAddr.new(ip_address).include?(IPAddr.new(request.ip)) }
-      anonymous_safelists << Safelist.new(nil, &ip_safelist_proc)
+      anonymous_safelists << Safelist.new(nil) { |request| IPAddr.new(ip_address).include?(IPAddr.new(request.ip)) }
     end
 
     def throttle(name, options, &block)
