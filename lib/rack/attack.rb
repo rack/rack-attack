9a7815c3322670fcc850bb7661e107eaa573f49a
Mitchell Henke <mitchell.henke@gsa.gov>
2023-10-14 09:29:19 -0500

    Remove support for unmaintained ActiveSupport::Cache::RedisStore (#625)
    * remove support for unmaintained ActiveSupport::Cache::RedisStore
    
    * ci: removes active_support_redis_store matrix job
    
    * test: removes unecessary redis-activesupport require
    
    * test: removes dead code
    
    * fix typo
    
    ---------
    
    Co-authored-by: Gonzalo <456459+grzuy@users.noreply.github.com>

--- lib/rack/attack.rb
+++ lib/rack/attack.rb
@@ -11,7 +11,6 @@ require 'rack/attack/store_proxy/mem_cache_store_proxy'
 require 'rack/attack/store_proxy/redis_proxy'
 require 'rack/attack/store_proxy/redis_store_proxy'
 require 'rack/attack/store_proxy/redis_cache_store_proxy'
-require 'rack/attack/store_proxy/active_support_redis_store_proxy'
 
 require 'rack/attack/railtie' if defined?(::Rails)
 
